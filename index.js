dbacfe4b7414c2352d92426262094c0ad3b0bfdc
VishwajeePatil <techtour1620@gmail.com>
2023-11-10 17:39:33 +0530

    Featured Section Added

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ const opnsdmenu = (flag)=>{
     let sidemenu = document.getElementById("side_menu")
     sidemenu.className = flag ? "active" : ""
 }
+let x = 0
+const scrollslider = (x)=>{
+        gsap.to(".slider .featuressection",{
+            left:(x),
+            duration:1.2,
+
+        })
+}
+
+const manualscrollslide = ()=>{
+    document.getElementById("feature_right").addEventListener("click",()=>{
+        if(x>-1020){
+            scrollslider(x-=395);
+        }
+    })
+    document.getElementById("feature_left").addEventListener("click",()=>{
+        if(x<0){
+            scrollslider(x+=395);
+        }
+    })
+}
+manualscrollslide();
+const automaticscrollslider = ()=>{
+        setInterval(()=>{
+            scrollslider(x-=395);
+            if(x<-1020){
+                return x=395;
+            }
+        },2000)
+}
+automaticscrollslider();
